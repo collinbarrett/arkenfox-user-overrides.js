4815dd43e3b95b5c8163f7ba11efe6d8426639fc
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-09-18 20:09:54 -0500

    init user-overrides with disabling pocket and pw manager

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ghacks-user-overrides.js
-My personal user-overrides.js file for the ghacks-user.js for Firefox
+My personal user-overrides.js file for the [ghacks-user.js](https://github.com/ghacksuserjs/ghacks-user.js) for Firefox
---
+++ user-overrides.js
@@ -0,0 +1,5 @@
+/*** MY OVERRIDES ***/
+user_pref("_user.js.parrot", "overrides section syntax error");
+user_pref("extensions.pocket.enabled", false);
+user_pref("signon.rememberSignons", false);
+user_pref("_user.js.parrot", "SUCCESS");
