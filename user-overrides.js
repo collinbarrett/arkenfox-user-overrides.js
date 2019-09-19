c472c0ea4f73bd2ee6f54a1e3be5ad703093995e
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-09-19 18:30:50 -0500

    improve docs

--- user-overrides.js
+++ user-overrides.js
@@ -1,6 +1,6 @@
 /*** MY OVERRIDES ***/
-/*** 1. Copy to Firefox profile */
-/*** 2. Run https://github.com/ghacksuserjs/ghacks-user.js/raw/master/updater.bat */
+/*** 1. Copy to Firefox profile directory */
+/*** 2. Run https://github.com/ghacksuserjs/ghacks-user.js/raw/master/updater.bat in Firefox profile directory */
 user_pref("_user.js.parrot", "overrides section syntax error");
 user_pref("extensions.pocket.enabled", false);
 user_pref("signon.rememberSignons", false);
