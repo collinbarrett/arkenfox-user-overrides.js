b9d5aa29a38c386c2fef9657b809d5ac7cdc3960
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2019-09-19 19:31:29 -0500

    enable searching from location bar

--- user-overrides.js
+++ user-overrides.js
@@ -2,6 +2,7 @@
 /*** 1. Copy to Firefox profile directory */
 /*** 2. Run https://github.com/ghacksuserjs/ghacks-user.js/raw/master/updater.bat in Firefox profile directory */
 user_pref("_user.js.parrot", "overrides section syntax error");
-user_pref("extensions.pocket.enabled", false);
-user_pref("signon.rememberSignons", false);
+user_pref("extensions.pocket.enabled", false); // 5000 disable Pocket extension
+user_pref("keyword.enabled", true); // 0801 enable searching from location bar
+user_pref("signon.rememberSignons", false); // 0901 disable password manager
 user_pref("_user.js.parrot", "SUCCESS");
