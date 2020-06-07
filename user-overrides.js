f3cfbf89f305f5c2ef25b080e65ef1ec028b74db
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-06-07 17:20:43 -0500

    rm dom.event.clipboardevents.enabled override commented in upstream

--- user-overrides.js
+++ user-overrides.js
@@ -2,7 +2,6 @@
 /*** 1. Copy to Firefox profile directory */
 /*** 2. Run https://github.com/ghacksuserjs/ghacks-user.js/raw/master/updater.bat in Firefox profile directory */
 user_pref("_user.js.parrot", "overrides section syntax error");
-user_pref("dom.event.clipboardevents.enabled", true); // 2402 enable copy/paste
 user_pref("privacy.resistFingerprinting.letterboxing", false); // 4504 disable letterboxing
 user_pref("extensions.pocket.enabled", false); // 5000 disable Pocket extension
 user_pref("keyword.enabled", true); // 0801 enable searching from location bar
