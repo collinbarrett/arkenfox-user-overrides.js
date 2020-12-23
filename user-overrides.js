b8d1304daacaa27734191d910b9dc0108c4a9a9c
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2020-12-23 17:11:26 +0000

    feat(webgl): ✨ enable webGL for Mapbox embeds

--- user-overrides.js
+++ user-overrides.js
@@ -7,3 +7,5 @@ user_pref("extensions.pocket.enabled", false); // 5000 disable Pocket extension
 user_pref("keyword.enabled", true); // 0801 enable searching from location bar
 user_pref("signon.rememberSignons", false); // 0901 disable password manager
 user_pref("_user.js.parrot", "SUCCESS");
+user_pref("webgl.disabled", false); // 2010 enable webGL for Mapbox embeds (e.g. NYT)
+user_pref("webgl.min_capability_mode", false); // 2012 enable webGL for Mapbox embeds (e.g. NYT)
