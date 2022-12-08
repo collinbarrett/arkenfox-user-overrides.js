f275761240f2d7ca36f983f5cb29253b10c33d43
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2022-12-07 18:47:05 -0600

    refactor: ♻️ re-add disable of rememberSignons

--- user-overrides.js
+++ user-overrides.js
@@ -6,4 +6,5 @@ user_pref("webgl.disabled", false); // 2010 enable webGL for Mapbox embeds (e.g.
 user_pref("privacy.resistFingerprinting.letterboxing", false); // 4504 disable letterboxing
 user_pref("extensions.pocket.enabled", false); // 5000 disable Pocket extension (not ref'd in ghacks user.js)
 user_pref("keyword.enabled", true); // 0801 enable searching from location bar
+user_pref("signon.rememberSignons", false); // 0901 disable password manager (not ref'd in ghacks user.js)
 user_pref("_user.js.parrot", "SUCCESS");
