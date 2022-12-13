5085a5022d9b923dc05226f427762c7287d171cf
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2022-12-13 15:31:12 +0000

    feat: ✨ commented loosen security key pinning for work antivirus

--- user-overrides.js
+++ user-overrides.js
@@ -2,6 +2,7 @@
 /*** 1. Copy to Firefox profile directory */
 /*** 2. Run https://github.com/ghacksuserjs/ghacks-user.js/raw/master/updater.bat in Firefox profile directory */
 user_pref("_user.js.parrot", "overrides section syntax error");
+//user_pref("security.cert_pinning.enforcement_level", 1); // 1223 loosen security key pinning for work antivirus
 user_pref("webgl.disabled", false); // 2010 enable webGL for Mapbox embeds (e.g. NYT)
 user_pref("privacy.resistFingerprinting.letterboxing", false); // 4504 disable letterboxing
 user_pref("extensions.pocket.enabled", false); // 5000 disable Pocket extension (not ref'd in ghacks user.js)
